a0b174272369048082845e5e06232ee7acf1f5b8
zeluliCredit <zeluli@creditease.cn>
2016-12-21 18:27:09 +0800

    add comment

--- FlyOver_Two/FlyOver/Views/BackView.swift
+++ FlyOver_Two/FlyOver/Views/BackView.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class BackView: UIView {
-    let codeCount = 150
+    let codeCount = 50
+    let range: CGFloat = 80
     var codeViews: Array<PointView> = []
     var beziers: Array<UIBezierPath> = []
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.black
+        self.addSubViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// 添加点
+    func addSubViews() {
         for i in 0..<codeCount {
             self.beziers.append(UIBezierPath())
             let codeView = PointView(frame: CGRect.zero)
             codeView.tag = i
             weak var weak_self = self
             codeView.setUpdateClosure(closure: { (index) in
-                    weak_self?.drawLine(index: index)
+                weak_self?.drawLine(index: index)
             })
             
             self.addSubview(codeView)
@@ -29,30 +39,9 @@ class BackView: UIView {
         }
     }
     
-    func areaPoints(index: Int) -> Array<CGPoint> {
-        var points = Array<CGPoint>()
-        points.append(self.codeViews[index].center)
-        for item in self.codeViews {
-            let distance = self.countDistance(point1: self.codeViews[index].center,
-                                              point2: item.center)
-            
-            if distance < 50 {
-                points.append(item.center)
-            }
-        }
-        return points
-    }
-    
-    func countDistance(point1: CGPoint, point2: CGPoint) -> CGFloat {
-        let x = pow(point1.x - point2.x, 2)
-        let y = pow(point1.y - point2.y, 2)
-        return CGFloat(sqrt(Double(x + y)))
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
+    /// 画线
+    ///
+    /// - Parameter index: 当前划线点的索引
     func drawLine(index: Int) {
         let points: Array<CGPoint> = self.areaPoints(index: index)
         var graph: Array<Array<Bool>>! = Array<Array<Bool>>(repeating: Array<Bool>(repeating: false, count: points.count), count: points.count)
@@ -72,18 +61,43 @@ class BackView: UIView {
         self.setNeedsDisplay()
     }
     
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    /// 计算在改点区域内的可连线的点
+    ///
+    /// - Parameter index: 当前移动的点
+    /// - Returns: 返回在规定范围内的点数组
+    func areaPoints(index: Int) -> Array<CGPoint> {
+        var points = Array<CGPoint>()
+        points.append(self.codeViews[index].center)
+        for item in self.codeViews {
+            let distance = self.countDistance(point1: self.codeViews[index].center,
+                                              point2: item.center)
+            if distance < self.range {
+                points.append(item.center)
+            }
+        }
+        return points
     }
     
+    /// 计算两点之间的距离
+    ///
+    /// - Parameters:
+    ///   - point1: 点1
+    ///   - point2: 点2
+    /// - Returns: 返回两点之间的距离
+    func countDistance(point1: CGPoint, point2: CGPoint) -> CGFloat {
+        let x = pow(point1.x - point2.x, 2)
+        let y = pow(point1.y - point2.y, 2)
+        return CGFloat(sqrt(Double(x + y)))
+    }
 
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         for j in 0..<self.beziers.count {
-            UIColor.white.setStroke()
-            self.beziers[j].lineWidth = 0.2
+            let weight = CGFloat(arc4random_uniform(1000))/1000
+            let color = UIColor(hue: weight, saturation: 1, brightness: 1, alpha: 1)
+            color.setStroke()
+            self.beziers[j].lineWidth = 0.3
             self.beziers[j].stroke()
         }
     }
--- FlyOver_Two/FlyOver/Views/PointView.swift
+++ FlyOver_Two/FlyOver/Views/PointView.swift
@@ -11,21 +11,18 @@ let ScreenSize = UIScreen.main.bounds.size
 let ScreenBounds = UIScreen.main.bounds
 typealias UpdateViewsClosure = (Int) -> Void
 class PointView: UIView {
+    
+    let widthAndHeight: CGFloat = 6
+    
     var x: CGFloat = 1
     var y: CGFloat = 1
-    let widthAndHeight: CGFloat = 3
     var updateViewClosure: UpdateViewsClosure! = nil
     var duration: Double = 0.0005
-    
     var source: DispatchSourceTimer!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.x = randomIncrement()
-        self.y = randomIncrement()
-        self.layer.cornerRadius = widthAndHeight / 2
-        self.backgroundColor = UIColor.white
-        self.frame = CGRect(origin: randomPoint(), size: CGSize(width: widthAndHeight, height: widthAndHeight))
-        self.duration = self.randomDuraion()
+        self.config()
     }
     
     override func layoutSubviews() {
@@ -33,16 +30,37 @@ class PointView: UIView {
         self.addDispatchSourceTimer()
     }
     
+    /// 配置当前View的默认属性
+    func config() {
+        self.x = randomIncrement()
+        self.y = randomIncrement()
+        self.layer.cornerRadius = widthAndHeight / 2
+        self.backgroundColor = UIColor.black
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+        self.frame = CGRect(origin: randomPoint(), size: CGSize(width: widthAndHeight, height: widthAndHeight))
+        self.duration = self.randomDuraion()
+    }
+
+    /// 设置更新回调
+    ///
+    /// - Parameter closure: <#closure description#>
     func setUpdateClosure(closure: @escaping UpdateViewsClosure) {
         self.updateViewClosure = closure
     }
     
+    /// 随机生成坐标点
+    ///
+    /// - Returns: <#return value description#>
     func randomPoint() -> CGPoint {
         arc4random_uniform(UInt32(ScreenSize.width))
         return CGPoint(x: Int(arc4random_uniform(UInt32(ScreenSize.width-widthAndHeight))),
                        y: Int(arc4random_uniform(UInt32(ScreenSize.height-widthAndHeight))))
     }
     
+    /// 随机生成x或者y的增量
+    ///
+    /// - Returns: <#return value description#>
     func randomIncrement() -> CGFloat {
         if arc4random() % 2 == 0 {
             return 1
@@ -51,7 +69,9 @@ class PointView: UIView {
         }
     }
     
-    fileprivate func addDispatchSourceTimer() {
+    
+    /// 添加Dispatch Source Timer
+    func addDispatchSourceTimer() {
         if source == nil {
              source = DispatchSource.makeTimerSource(flags: DispatchSource.TimerFlags(rawValue: UInt(0)), queue: DispatchQueue.main) as? DispatchSource
         }
@@ -63,9 +83,11 @@ class PointView: UIView {
         }
         source.resume()
     }
-
     
+    /// 改变当前View的坐标
     func changePoint() {
+        let weight = CGFloat(arc4random_uniform(1000))/1000
+        self.backgroundColor = UIColor(hue: weight, saturation: 1, brightness: 1, alpha: 1)
         var currentPoint = self.frame.origin
         
         if currentPoint.x > ScreenSize.width-self.widthAndHeight || currentPoint.x < 0 {
@@ -85,8 +107,11 @@ class PointView: UIView {
         }
     }
     
+    /// 随机生成动画时间
+    ///
+    /// - Returns: <#return value description#>
     func randomDuraion() -> Double {
-        return Double(arc4random_uniform(10))/10
+        return Double(arc4random_uniform(100))/100
     }
     
     required init?(coder aDecoder: NSCoder) {
